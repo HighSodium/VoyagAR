bdd01040b2e8e696d3a9fc8501251ae8167b210d
Bayron <norybyte@gmail.com>
2021-01-30 16:46:59 -0500

    Merge branch 'main' of https://github.com/HighSodium/VoyagAR into main

---
+++ Assets/Scripts/WeatherManager/WeatherManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using TMPro;
+using UnityEngine.Networking;
+
+public class WeatherManager : MonoBehaviour
+{
+    //public string weatherAPI = "api.openweathermap.org/data/2.5/weather?q=London,uk&appid=16b632699a5b299cfd0723abf35b5b3a";
+    public TextMeshProUGUI location;
+    public TextMeshProUGUI weather;
+    //public TextMeshProUGUI mainWeather;
+    //public TextMeshProUGUI description;
+    public TextMeshProUGUI geoLocation;
+
+    public TMP_InputField cityName; // User input in UI
+
+    public void GetJsonData()
+    {
+        StartCoroutine(RequestWeatherData());
+    }
+
+    IEnumerator RequestWeatherData()
+    {
+        string weatherAPI = "api.openweathermap.org/data/2.5/weather?q=" + cityName.text + "&appid=16b632699a5b299cfd0723abf35b5b3a";
+        print(weatherAPI);
+
+        using (UnityWebRequest webData = UnityWebRequest.Get(weatherAPI))
+        {
+            yield return webData.SendWebRequest();
+            if (webData.isNetworkError || webData.isHttpError)
+            {
+                print("ERROR");
+                print(webData.error);
+            }
+            else
+            {
+                if (webData.isDone)
+                {
+                    var response = JSON.Parse(webData.downloadHandler.text);
+
+                    if (response == null)
+                    {
+                        print("NO DATA");
+                    }
+                    else
+                    {
+                        location.text = response["name"];
+                        weather.text = response["main"];
+                        geoLocation.text = response["coord"]["lon"]["lat"];
+                    }
+                }
+            }
+         
+        }
+    }
+}
